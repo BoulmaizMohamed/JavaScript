61ff269078cc7daf11b68bf8bef4ab5443f65da2
BoulmaizMohamed <90794998+BoulmaizMohamed@users.noreply.github.com>
2023-04-05 22:55:51 +0100

    Create Conditional Statements.js

---
+++ Conditional Statements.js
@@ -0,0 +1,26 @@
+// if statement
+let age = 20;
+if (age >= 18) {
+  console.log("You are an adult.");
+}
+
+// if-else statement
+let temperature = 25;
+if (temperature > 30) {
+  console.log("It's too hot!");
+} else {
+  console.log("It's comfortable.");
+}
+
+// switch statement
+let day = "Monday";
+switch (day) {
+  case "Monday":
+    console.log("Today is Monday.");
+    break;
+  case "Tuesday":
+    console.log("Today is Tuesday.");
+    break;
+  default:
+    console.log("Today is not Monday or Tuesday.");
+}
