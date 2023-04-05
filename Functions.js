75608dd81d5a33cacf3eea39a8a301c1ea14783b
BoulmaizMohamed <90794998+BoulmaizMohamed@users.noreply.github.com>
2023-04-05 22:56:48 +0100

    Create Functions.js

---
+++ Functions.js
@@ -0,0 +1,15 @@
+// declaring a function
+function greet(name) {
+  console.log("Hello, " + name + "!");
+}
+
+// calling a function
+greet("John");
+
+// function with return value
+function sum(a, b) {
+  return a + b;
+}
+
+let result = sum(3, 4);
+console.log(result);
