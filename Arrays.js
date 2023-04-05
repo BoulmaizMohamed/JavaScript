b2727b269f23a1a1a532c11543cfbe58b090bbb3
BoulmaizMohamed <90794998+BoulmaizMohamed@users.noreply.github.com>
2023-04-05 22:57:14 +0100

    Create Arrays.js

---
+++ Arrays.js
@@ -0,0 +1,12 @@
+// declaring an array
+let fruits = ["apple", "banana", "orange"];
+
+// accessing array elements
+console.log(fruits[0]); // apple
+
+// adding elements to an array
+fruits.push("grape"); // adds "grape" to the end of the array
+fruits.unshift("mango"); // adds "mango" to the beginning of the array
+
+// removing elements from an array
+fruits.pop(); // removes the last element
